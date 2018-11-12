ade9a3ae3d6183414f46aa134c83067b5e886c20
Stephen P <stephenp@ac-bc-32-9f-e5-53.wireless.temple.edu>
2018-11-12 00:55:57 -0500

    comp for

--- reverse.js
+++ reverse.js
@@ -1,9 +1,22 @@
 var reverse = function(){
 
     var string = prompt("eneter a string");
-    //code here
+   
+    var wordsArr = string.split(" ");
+    var reversedWordsArr= [];
+
+    for(var x = 0; x< wordsArr.length; x++){
+        var reversedWord = "";
+        for(var y = wordsArr[x].length - 1; y >= 0; y--){
+  
+            reversedWord += wordsArr[x][y];
+        }
+        reversedWordsArr.push(reversedWord);
+
+    }
 
 
+    document.getElementById("reversedtext").innerText = reversedWordsArr.join(" ");
 
 
 }
